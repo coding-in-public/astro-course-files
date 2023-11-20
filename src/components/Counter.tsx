125b96cbf747d7022aa44632e2efa2412fb49688
Chris Pennington <chris@codinginpublic.dev>
2023-11-20 09:08:48 -0700

    lesson-13

---
+++ src/components/Counter.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
