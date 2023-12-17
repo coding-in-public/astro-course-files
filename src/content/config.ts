a6e92631bafb49d2c1cd7070f8e08d6f931c44f5
Chris Pennington <chris@codinginpublic.dev>
2023-12-17 06:17:33 -0700

    lesson-25

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,20 @@ const team = defineCollection({
   }),
 });
 
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string().max(65, {
+      message: "Your title must be 65 characters or less.",
+    }),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    isDraft: z.boolean(),
+    canonicalURL: z.string().url().optional(),
+  }),
+});
+
 export const collections = {
   team,
+  posts,
 };
---
+++ src/content/posts/post-1.md
@@ -0,0 +1,9 @@
+---
+title: Post 1
+tags: ["product", "tutorial"]
+pubDate: 2023-12-08
+isDraft: false
+canonicalURL: https://mysite.com/sample/
+---
+
+Content
