22097909afe432be5022fad991fbe6a54e4fd379
Chris Pennington <chris@codinginpublic.dev>
2024-01-04 14:50:41 -0700

    lesson 31

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
+});
