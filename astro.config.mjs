833f634c15c5920bc9dd2832860fc12593bb15d0
Chris Pennington <chris@codinginpublic.dev>
2024-01-05 07:11:10 -0700

    lesson-35

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
-});
+export default defineConfig({});
