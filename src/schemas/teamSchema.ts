95a4bd5fe4a54e120c1afb9b9bbc549ccb02f451
Chris Pennington <chris@codinginpublic.dev>
2024-02-15 10:27:34 -0700

    lesson-39

--- src/schemas/teamSchema.ts
+++
@@ -1,9 +0,0 @@
-import { reference, z } from "astro:content";
-
-const teamSchema = z.object({
-  name: z.string(),
-  role: reference("role"),
-  location: z.string().optional(),
-});
-
-export default teamSchema;
