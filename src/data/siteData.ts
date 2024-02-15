37b53a82fbab24a5fce0f4c9e812c9f710610c18
Chris Pennington <chris@codinginpublic.dev>
2024-02-15 11:01:04 -0700

    lesson-40

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: "Katie Haus Portfolio Site",
+  description:
+    "Welcome to my portfolio site! I’m a self-taught designer and developer obsessed with quality.",
+};
