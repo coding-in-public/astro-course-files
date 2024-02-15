926f18db7409e966ae7fdc2b94fcee53887a8377
Chris Pennington <chris@codinginpublic.dev>
2024-02-15 11:11:52 -0700

    lesson-41

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()]
+});
