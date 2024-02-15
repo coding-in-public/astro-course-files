003e6248e2c5f19c2edd0cb2435cfa2962c56272
Chris Pennington <chris@codinginpublic.dev>
2024-02-15 11:23:01 -0700

    lesson-42

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -3,3 +3,31 @@ export const baseData = {
   description:
     "Welcome to my portfolio site! I’m a self-taught designer and developer obsessed with quality.",
 };
+
+export const socialData = [
+  {
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "icon--dribbble",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://youtube.com/",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "icon--github",
+  },
+];
