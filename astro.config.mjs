e5d133c6b20435dc9b1c63b6ebd8a6e3a7271a42
Chris Pennington <chris@codinginpublic.dev>
2024-02-16 11:54:56 -0700

    lesson-64

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  prefetch: {
+    prefetchAll: true,
+  },
+  site: "https://stargazers.club",
+  integrations: [icon(), sitemap()],
   output: "hybrid",
   adapter: netlify(),
 });
