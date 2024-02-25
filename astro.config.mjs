81a89057e8a4c03bd99a592744cb531975ec01e6
Chris Pennington <chris@codinginpublic.dev>
2024-02-25 14:39:09 -0700

    lesson-68

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["astro.build"],
+    domains: ["astro.build"]
   },
+  integrations: [mdx()]
 });
