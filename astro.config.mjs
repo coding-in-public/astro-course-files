3ec41f7fc5720260b98d6cb98c0a7bcfdf49d3c6
Chris Pennington <chris@codinginpublic.dev>
2024-03-17 13:16:17 -0600

    lesson-82-prep

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon(), tailwind()],
+});
